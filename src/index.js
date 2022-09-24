ae4a0c723581a269ad54acc806a9fc92e70bbf80
daryna-s <lypenn@gmail.com>
2022-09-24 19:05:37 +0300

    add library

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+ const API_KEY = '30148618-cb32e0aec2d1b32c973ff6349';
+ const BASE_URL = 'https://pixabay.com/api/';
